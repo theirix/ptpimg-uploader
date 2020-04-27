f6e273a30c46ae5c66f1ac3de158e8287fcd6351
theirix <theirix@gmail.com>
2020-04-27 23:16:39 +0300

    Version 0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as desc:
 
 setup(
     name="ptpimg_uploader",
-    version="0.3",
+    version="0.4",
     author="theirix",
     author_email="theirix@gmail.com",
     description=(
